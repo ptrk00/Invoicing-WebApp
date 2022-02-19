a6fc854764a6584eaca0e0dac19164500e9ececc
ptrk00 <76401343+Piotr12309@users.noreply.github.com>
2022-02-19 17:07:27 +0100

    Fix undefined param in auth-helper function.

--- api/controllers/clientController.js
+++ api/controllers/clientController.js
@@ -5,7 +5,7 @@ const Client = require('../models/client');
 exports.authorizeClientAccess = async (req,res, next) => {
 
     const userId = req.userId.id;
-    const clientId = req.params.id;
+    const clientId = req.params.clientId;
 
     // check if the client to be found is in the user client's list
     const doc = await User.findOne({
