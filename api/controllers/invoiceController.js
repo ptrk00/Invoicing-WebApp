7af7b2b0994bd42d117a16f0c45063262e68c725
ptrk00 <76401343+Piotr12309@users.noreply.github.com>
2022-02-20 13:13:34 +0100

    Add invoice controller CRUD methods.

--- api/controllers/invoiceController.js
+++ api/controllers/invoiceController.js
@@ -43,7 +43,6 @@ exports.addInvoice = async (req,res) => {
 
 exports.getInvoice = async (req,res) => {
 
-    const clientId = req.params.clientId;
     const invoiceId = req.params.invoiceId;
 
     try {
@@ -53,4 +52,63 @@ exports.getInvoice = async (req,res) => {
         console.log(err);
         res.sendStatus(500);
     }
+}
+
+exports.getAllInvoicesFromOneClient = async (req,res) => {
+
+    const clientId = req.params.clientId;
+
+    try {
+        const client = await Client.findById(clientId).populate('invoices').exec();
+        const invoices = [...client._doc.invoices];
+        res.status(200).json(invoices);
+    } catch(err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+
+}
+
+exports.updateInvoice = async (req,res) => {
+
+    const invoiceId = req.params.invoiceId;
+
+    try {
+        const invoice = await Invoice.findByIdAndUpdate(invoiceId, {$set: req.body}, {returnDocument: 'after'}).exec();
+        res.status(200).json(invoice);
+    } catch(err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+
+}
+
+exports.deleteInvoice = async (req,res) => {
+
+    const invoiceId = req.params.invoiceId;
+
+    try {
+        await Invoice.findByIdAndDelete(invoiceId).exec();
+        res.sendStatus(200);
+    } catch(err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+
+}
+
+// TODO: fix
+exports.getAllInvoicesFromAllClients = async (req,res) => {
+
+    const userId = req.userId.id;
+
+    try {
+        const user = await User.findById(userId).populate('clients').exec();
+        const invoices = Invoice.find({client: {$in: user._doc.clients.map(c => c._doc.invoices._id) }}).exec();
+        res.status(200).json(invoices._doc);
+    } catch(err) {
+      console.log(err);
+      res.sendStatus(500);
+    }
+
 }
--- api/routes/invoiceRouote.js
+++ api/routes/invoiceRouote.js
@@ -4,10 +4,12 @@ const authenticateToken = require('../auth/tokenAuth');
 const {authorizeClientAccess} = require("../controllers/clientController");
 const {authorizeInvoiceAccess} = require("../controllers/invoiceController");
 
+router.get('/all/invoice',authenticateToken, invoiceController.getAllInvoicesFromAllClients);
 router.post('/:clientId/invoice', authenticateToken, authorizeClientAccess,invoiceController.addInvoice);
 router.get('/:clientId/invoice/:invoiceId', authenticateToken, authorizeClientAccess, authorizeInvoiceAccess, invoiceController.getInvoice);
-// router.get('/', authenticateToken, invoiceController.getAllClients);
-// router.delete('/:id', authenticateToken,  invoiceController.authorizeClientAccess, invoiceController.deleteClient);
-// router.put('/:id', authenticateToken,  invoiceController.authorizeClientAccess, invoiceController.updateClient);
+router.get('/:clientId/invoice', authenticateToken, authorizeClientAccess, invoiceController.getAllInvoicesFromOneClient);
+router.put('/:clientId/invoice/:invoiceId', authenticateToken, authorizeClientAccess, authorizeInvoiceAccess, invoiceController.updateInvoice);
+router.delete('/:clientId/invoice/:invoiceId', authenticateToken, authorizeClientAccess, authorizeInvoiceAccess, invoiceController.deleteInvoice);
+
 
 module.exports = router;
